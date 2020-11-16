8d0aa69b059e0d58e8351876172b2887d9e7b14e
heltan <helen.tanubrata@sfcm.edu>
2020-11-16 13:24:12 -0800

    Added player tracker and alternating X and O

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 
 let table = document.getElementById("table");
 let cells = table.getElementsByTagName("td");
+let currentPlayerisX = true;
 for (let i = 0; i < cells.length; i ++) {
   cells[i].onclick = () => {
-    cells[i].innerHTML = 'X';
+    if (currentPlayerisX) {
+      cells[i].innerHTML = '0';
+      currentPlayerisX = false;
+    } else if (!currentPlayerisX) {
+      currentPlayerisX = true;
+      cells[i].innerHTML = 'X';
+    }
+
   }
 };
 
