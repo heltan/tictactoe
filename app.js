15544442349bf2afc9a2af4e16d91faa2a0a5150
heltan <helen.tanubrata@sfcm.edu>
2020-11-16 11:28:16 -0800

    setting up onclick to add X on user click in table cell

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-//set up an onclick for the table
-document.getElementById("table").onclick = () => {
-  alert('this is clicked');
-}
+//set up an onclick for the table Cell
+
+let table = document.getElementById("table");
+let cells = table.getElementsByTagName("td");
+for (let i = 0; i < cells.length; i ++) {
+  cells[i].onclick = () => {
+    cells[i].innerHTML = 'X';
+  }
+};
+
