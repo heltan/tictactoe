f7b3fe6f4dbd0eedadaa27d8e077ee6c4bdef2be
heltan <helen.tanubrata@sfcm.edu>
2020-11-16 19:25:41 -0800

    adding X or O when clicked, and keeping track of player in local storage

--- app.js
+++ app.js
@@ -2,15 +2,33 @@
 
 let table = document.getElementById("table");
 let cells = table.getElementsByTagName("td");
-let currentPlayerisX = true;
+//first we check if anything is saved in local storage for who the current player is
+let getCurrentPlayer = localStorage.getItem('CurrentPlayerX');
+if (getCurrentPlayer === undefined) {
+  //if no current player, then we set
+  localStorage.setItem('CurrentPlayerX', 'true');
+} else {
+  //otherwise don't do anything
+  localStorage.setItem('CurrentPlayerX', 'true');
+}
+let cellHTML;
+//now we set onclick for every cell in the table
 for (let i = 0; i < cells.length; i ++) {
+
   cells[i].onclick = () => {
-    if (currentPlayerisX) {
-      cells[i].innerHTML = '0';
-      currentPlayerisX = false;
-    } else if (!currentPlayerisX) {
-      currentPlayerisX = true;
+    //we check who the current player is from local storage
+    getCurrentPlayer = localStorage.getItem('CurrentPlayerX');
+
+    //now we test if current player is X, and the cell they click on is available to click
+    if (getCurrentPlayer === 'true' && cells[i].innerHTML === 'click here') {
+      console.log('going in here');
       cells[i].innerHTML = 'X';
+      localStorage.setItem('CurrentPlayerX', 'false');
+
+    } else if (getCurrentPlayer === 'false' && cells[i].innerHTML === 'click here') {
+
+      cells[i].innerHTML = 'O';
+      localStorage.setItem('CurrentPlayerX', 'true');
     }
 
   }
