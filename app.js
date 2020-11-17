c25ab8a0a7f24d2c4c521607c50e0e1736e6b39b
heltan <helen.tanubrata@sfcm.edu>
2020-11-17 09:12:42 -0800

    cleaning up code removing console lot

--- app.js
+++ app.js
@@ -13,13 +13,8 @@ document.getElementById('newgame').onclick = () => {
   }
   gamePlay();
 };
-
-
-
-
 //this is the game play function that controls all the game
 let gamePlay = () => {
-
   //let table = document.getElementById("table");
   let cells = table.getElementsByTagName("td");
   //first we check if anything is saved in local storage for who the current player is
@@ -31,29 +26,20 @@ let gamePlay = () => {
     //otherwise don't do anything
     localStorage.setItem('CurrentPlayerX', 'true');
   }
-  let cellHTML;
-
   //now we set onclick for every cell in the table
   //and we will push all the inputs into a gamearray, array.
   gameArray =[1,1,1,1,1,1,1,1,1];
   for (let i = 0; i < cells.length; i ++) {
-    //console.log(cells, 'cells');
-
     cells[i].onclick = () => {
       //we check who the current player is from local storage
       getCurrentPlayer = localStorage.getItem('CurrentPlayerX');
       let gameOver = localStorage.getItem('GameOver');
       //now check if there are empty spaces or not
-
-
       //now we test if current player is X, and the cell they click on is available to click
       if (gameOver!== 'true' && getCurrentPlayer === 'true' && cells[i].innerHTML === 'click here') {
-
         cells[i].innerHTML = 'X';
         gameArray[i] = 'X';
         localStorage.setItem('CurrentPlayerX', 'false');
-
-
       } else if (gameOver !== 'true' && getCurrentPlayer === 'false' && cells[i].innerHTML === 'click here') {
 
         cells[i].innerHTML = 'O';
@@ -72,7 +58,6 @@ let gamePlay = () => {
         document.getElementById('status').innerHTML = `Winner is ${winner}!`;
         //now we will stop the gameplay
         localStorage.setItem('GameOver', 'true');
-
       }
       //now we check vertical check for game over if there is no horizontal winner
       if (!winner) {
@@ -94,7 +79,6 @@ let gamePlay = () => {
           winner = undefined;
           gameArray =[1,1,1,1,1,1,1,1,1];
           localStorage.clear();
-
         } else {
           //now we check if it's a tie by determining if all spaces have been used
           let check = checkSpaces();
@@ -104,22 +88,13 @@ let gamePlay = () => {
             winner = undefined;
             gameArray =[1,1,1,1,1,1,1,1,1];
             localStorage.clear();
-
-
           }
         }
       }
-
-
-
-
     }
   }
-
-
 };
 gamePlay();
-
 //this is a function that takes our game array and splits it into 3 arrays to be used by the below functions
 let splitThree = (array) => {
   let mainArray = [];
@@ -128,34 +103,26 @@ let splitThree = (array) => {
   let arr3 = array.slice(6,9);
   mainArray.push(arr1, arr2, arr3);
   return mainArray;
-
 };
-
 //here are functions that check if there is a winner
 //they will return the 'winner' variable if there is a winner. it will equal X or O
-
-
 let winnerArray;
 let checkHorizontal = (array) => {
   //we will check each array inside, vertically. for every row
-
   for (let i = 0; i < array.length; i ++) {
       let currentArr = array[i];
       for (let a = 0; a < array.length; a ++) {
           if (currentArr[0] !== 1 && currentArr[0] === currentArr[1] && currentArr[0] === currentArr[2]) {
               winner = currentArr[0];
-
               return;
           }
       }
   }
 };
 //now below checks vertically for winner
-
 let checkVertical = (array) => {
     let array1 = array[0];
     //only need to check through first row for vertically
-
     for (let i = 0; i < array[0].length; i++) {
         if (array1[i] !== 1) {
              let currentCheck = array1[i]; //this will be X, or O
@@ -163,14 +130,10 @@ let checkVertical = (array) => {
              let arrToCheck = array[1];
              if (currentCheck === arrToCheck[i] && currentCheck === array[2][i]) {
                  winner = currentCheck;
-                 console.log('vertical winner ', array1);
                  return;
              }
         }
-
-
     }
-
 };
 //now a function to check diagonally
 let checkDiagonal = function (array) {
@@ -179,30 +142,23 @@ let checkDiagonal = function (array) {
     let first = array[0][0];
     let last = array[0][2];
     let mid = array[1][1];
-
     let firstLastRow = array[2][0];
     let lastLastRow = array[2][2];
-    console.log('first', first, 'last', last, 'mid', mid, 'firstlastrow', firstLastRow, 'lastlastrow', lastLastRow);
     if (first !== 1 && first === mid && first === lastLastRow) {
         winner = first;
-
         return;
     } if (last !== 1 && last === mid && last === firstLastRow) {
         winner = last;
         return;
     }
-
 };
 //now a function to determine if all spaces on the board have been used
 let checkSpaces = () => {
   let cells = table.getElementsByTagName("td");
-
   for (let i = 0; i < cells.length; i ++) {
     if (cells[i].innerHTML === 'click here') {
       return false;
     }
-
   }
   return true;
-
 };
